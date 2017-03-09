5c1cf202430345866a159b4abc48d18066b62002
zqhong <i@zqhong.com>
2017-03-09 13:09:53 +0800

    added：添加简单的示例代码

---
+++ examples/simple.php
@@ -0,0 +1,29 @@
+<?php
+
+use zqhong\route\Helpers\Arr;
+use zqhong\route\RouteCollector;
+use zqhong\route\RouteDispatcher;
+
+require dirname(__DIR__) . "/vendor/autoload.php";
+
+function getUser($uid)
+{
+    echo "Your uid: " . $uid;
+}
+
+/** @var RouteDispatcher $routeDispatcher */
+$routeDispatcher = dispatcher(function (RouteCollector $r) {
+    $r->addRoute('GET', '/user/{id:\d+}', 'getUser');
+});
+
+$httpMethod = 'GET';
+$uri = '/user/1';
+$routeInfo = $routeDispatcher->dispatch($httpMethod, $uri);
+
+if (Arr::getValue($routeInfo, 'isFound')) {
+    $handler = Arr::getValue($routeInfo, 'handler');
+    $params = Arr::getValue($routeInfo, 'params');
+    call_user_func_array($handler, $params);
+} else {
+    exit('404 NOT FOUND');
+}
